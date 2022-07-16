d178ac55dbc431fedc4b7cef3e0d04542dd3e8dc
Andres032 <andrezztorregrosa@gmail.com>
2022-07-16 12:16:34 -0500

    Realization of user model and task and state

---
+++ src/enum/status.js
@@ -0,0 +1,9 @@
+const Etask= {
+    'pendiente' : 0,
+    'realizado' : 1,
+}
+
+
+module.exports = {
+    Etask
+}
---
+++ src/models/Tarea.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+import { Etask } from '../enum/status';
+
+const TareaSchema = Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+    start: {
+        type: Date,
+        required: true
+    },
+    end: {
+        type: Date,
+        required: true
+    },
+    estado: {
+        type: Number,
+        default: Etask.pendiente,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true
+    }
+
+});
+
+TareaSchema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+
+
+
+module.exports = model('Tarea', TareaSchema);
