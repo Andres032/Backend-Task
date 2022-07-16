f15295ca2870759d3880e5ff8b31ed38fd44fdb7
Andres032 <andrezztorregrosa@gmail.com>
2022-07-16 09:40:52 -0500

    Configuring the server

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+
+// Crear el servidor de express
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
+
+
+// CORS
+app.use(cors())
+
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+
+
+
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
+
